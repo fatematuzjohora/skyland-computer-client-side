e43a48228fcf70997b5cf1ff48340e7c5b8d54da
fatematuzjohora <fatematuzjohora889@gmail.com>
2022-05-08 08:24:16 +0600

    singUp page impliment

--- src/components/Authentication/SignUp/SignUp.js
+++ src/components/Authentication/SignUp/SignUp.js
@@ -1,17 +1,52 @@
 import React from "react";
+import {
+  useCreateUserWithEmailAndPassword,
+  useUpdateProfile,
+} from "react-firebase-hooks/auth";
+import { Link, useNavigate } from "react-router-dom";
+import auth from "../../../firebase.init";
+import Loading from "../../Loading/Loading";
 
 const SignUp = () => {
+  const navigate = useNavigate();
+
+  const [createUserWithEmailAndPassword, user, loading, error] =
+    useCreateUserWithEmailAndPassword(auth, { sendEmailVerification: true });
+  const [updateProfile, updating] = useUpdateProfile(auth);
+  let errorMessage;
+
+  if (loading || updating) {
+    return <Loading></Loading>;
+  }
+  if (user) {
+    console.log("user", user);
+  }
+  if (error) {
+    errorMessage = <p className="text-danger">{error?.message} </p>;
+  }
+
+  const handleFormRegister = async (event) => {
+    event.preventDefault();
+    const name = event.target.name.value;
+    const email = event.target.email.value;
+    const password = event.target.password.value;
+
+    await createUserWithEmailAndPassword(email, password);
+    await updateProfile({ displayName: name });
+    console.log("Updated profile");
+    navigate("/home");
+  };
   return (
     <div className=" py-5 bg-slate-400 h-screen">
-      <div className=" border-2 py-6 md:mx-96 rounded-xl  bg-slate-200">
+      <div className=" border-2 items-center flex flex-col py-6 md:mx-96 rounded-xl  bg-slate-200">
         <h5 className=" border-cyan-300 font-serif m-3 border-2 rounded-xl  text-xl mb-8 font-medium text-gray-900 dark:text-white">
           SignUp to our platform
         </h5>
-        <form>
+        <form onSubmit={handleFormRegister}>
           <div>
             <label
               htmlFor=""
-              className=" mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+              className=" mt-2 text-sm font-medium text-gray-900 dark:text-gray-300"
             >
               Your Name
             </label>{" "}
@@ -28,7 +63,7 @@ const SignUp = () => {
           <div>
             <label
               htmlFor=""
-              className=" mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+              className=" mt-2 text-sm font-medium text-gray-900 dark:text-gray-300"
             >
               Your Email
             </label>{" "}
@@ -45,7 +80,7 @@ const SignUp = () => {
           <div>
             <label
               htmlFor=""
-              className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+              className="block mt-2 text-sm font-medium text-gray-900 dark:text-gray-300"
             >
               Your Password
             </label>
@@ -58,11 +93,15 @@ const SignUp = () => {
             />{" "}
             <br />
           </div>
-          <div className="">
-            <p className="ml-auto mb-4 text-sm text-blue-700 hover:underline dark:text-blue-500">
-              already have an account?
-            </p>
+          <div className="my-4">
+            <Link
+              to={"/login"}
+              className="ml-auto text-sm text-blue-700 hover:underline dark:text-blue-500"
+            >
+              <button>already have an account?</button>
+            </Link>
           </div>
+          {errorMessage}
           <button
             type="submit"
             className=" w-60 mb-3 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -1,13 +1,38 @@
-import { Spinner } from "flowbite-react";
 import React from "react";
 const Loading = () => {
   return (
-    <div
-      style={{ height: "300px" }}
-      className="w-full flex justify-center items-center"
-    >
-      <Spinner color="yellow" size="sm" />
+    <div class="flex justify-center items-center space-x-2">
+    <div class="spinner-grow inline-block w-8 h-8 bg-current rounded-full opacity-0 text-blue-600" role="status">
+      <span class="visually-hidden">Loading...</span>
     </div>
+    <div class="
+        spinner-grow inline-block w-8 h-8 bg-current rounded-full opacity-0
+          text-purple-500
+        " role="status">
+      <span class="visually-hidden">Loading...</span>
+    </div>
+    <div class="
+        spinner-grow inline-block w-8 h-8 bg-current rounded-full opacity-0
+          text-green-500
+        " role="status">
+      <span class="visually-hidden">Loading...</span>
+    </div>
+    <div class="spinner-grow inline-block w-8 h-8 bg-current rounded-full opacity-0 text-red-500" role="status">
+      <span class="visually-hidden">Loading...</span>
+    </div>
+    <div class="
+        spinner-grow inline-block w-8 h-8 bg-current rounded-full opacity-0
+          text-yellow-500
+        " role="status">
+      <span class="visually-hidden">Loading...</span>
+    </div>
+    <div class="spinner-grow inline-block w-8 h-8 bg-current rounded-full opacity-0 text-blue-300" role="status">
+      <span class="visually-hidden">Loading...</span>
+    </div>
+    <div class="spinner-grow inline-block w-8 h-8 bg-current rounded-full opacity-0 text-gray-300" role="status">
+      <span class="visually-hidden">Loading...</span>
+    </div>
+  </div>
   );
 };
 
