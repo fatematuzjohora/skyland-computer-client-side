9f416850b828c2c645c8ee637660b1f2d0df6df3
fatematuzjohora <fatematuzjohora889@gmail.com>
2022-05-08 08:29:21 +0600

    login page impliment

--- src/components/Authentication/Login/Login.js
+++ src/components/Authentication/Login/Login.js
@@ -1,12 +1,57 @@
-import { sendPasswordResetEmail } from "firebase/auth";
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useRef } from "react";
+import { useSendPasswordResetEmail, useSignInWithEmailAndPassword } from "react-firebase-hooks/auth";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import { toast, ToastContainer } from "react-toastify";
+import auth from "../../../firebase.init";
+import Loading from "../../Loading/Loading";
 import SocialLogin from "../SocialLogin/SocialLogin";
 
 const Login = () => {
-  const resetPassword = async (event) => {
-    const email = event.name.value;
+  const navigate = useNavigate();
+  const location = useLocation();
+  const emailRef = useRef("");
+
+  let from = location.state?.from?.pathname || "/home";
+  const [signInWithEmailAndPassword, user, loading, error] =
+    useSignInWithEmailAndPassword(auth);
+  const [sendPasswordResetEmail, sending] = useSendPasswordResetEmail(auth);
+  let errorMessage;
+
+  if (loading || sending) {
+    return <Loading></Loading>;
+  }
+  if (user) {
+    fetch("https://ns-mobile-house.herokuapp.com/login", {
+      method: "POST",
+      body: JSON.stringify({
+        email: user?.user?.email,
+      }),
+      headers: {
+        "Content-type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        localStorage.setItem("accessToken", data?.token);
+        toast.success("Welcome To ns Showroom");
+        navigate(from, { replace: true });
+      });
+  }
+  if (error) {
+    errorMessage = <p className="text-danger">{error?.message} </p>;
+  }
+
+  const handleLogin = (event) => {
+    event.preventDefault();
+    const email = event.target.email.value;
+    const password = event.target.password.value;
+    console.log(email, password);
+
+    signInWithEmailAndPassword(email, password);
+  };
+
+  const resetPassword = async () => {
+    const email = emailRef.current.value;
     if (email) {
       await sendPasswordResetEmail(email);
       toast("Sent email");
@@ -16,12 +61,12 @@ const Login = () => {
   };
   return (
     <div className=" py-5 bg-slate-400 h-screen">
-      <div className=" border-2 py-6 md:mx-96 rounded-xl  bg-slate-200">
-        <h5 className=" border-cyan-300 font-serif m-3 border-2 rounded-xl text-xl mb-8 font-medium text-gray-900 dark:text-white">
+      <div className=" border-2 items-center flex flex-col py-6 md:mx-96 rounded-xl  bg-slate-200">
+        <h5 className=" border-cyan-300 font-serif m-3 text-center border-2 rounded-xl text-xl mb-8 font-medium text-gray-900 dark:text-white">
           SignIn to our platform
         </h5>
-        <form>
-          <div>
+        <form onSubmit={handleLogin}>
+          <div className="  ">
             <label
               htmlFor=""
               className=" mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
@@ -30,6 +75,7 @@ const Login = () => {
             </label>{" "}
             <br />
             <input
+              ref={emailRef}
               type="email"
               name="email"
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-60 p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
@@ -41,7 +87,7 @@ const Login = () => {
           <div className="mb-4">
             <label
               htmlFor=""
-              className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+              className="block mt-2 text-sm font-medium text-gray-900 dark:text-gray-300"
             >
               Your Password
             </label>
@@ -62,26 +108,25 @@ const Login = () => {
             Login to your account
           </button>
         </form>
-        <div className=" flex p-5 flex-row justify-between">
-          <div className="">
-            <p
-              onClick={resetPassword}
-              className=" text-sm text-blue-700 hover:underline dark:text-blue-500"
-            >
+        {errorMessage}
+        <div className=" flex p-5 justify-between">
+          <p className=" text-sm mr-3 text-blue-700 hover:underline dark:text-blue-500">
+            <button onClick={resetPassword} type="reset">
               Lost Password?
-            </p>
-          </div>
-
+            </button>
+          </p>{" "}
           <Link
-            to="/singUp"
-            className=" text-sm text-blue-700 hover:underline dark:text-blue-500"
+            to="/signUp"
+            className=" text-sm ml-3 text-blue-700 hover:underline dark:text-blue-500"
           >
             New? Please Register
           </Link>
         </div>
-        <SocialLogin></SocialLogin>
-        <ToastContainer />
+        <div>
+          <SocialLogin></SocialLogin>
+        </div>
       </div>
+      <ToastContainer />
     </div>
   );
 };
